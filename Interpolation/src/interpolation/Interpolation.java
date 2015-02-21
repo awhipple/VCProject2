df317b886ab7371d13c923470bbcb5816526cfda
awhipple <a.rgwhip@gmail.com>
2015-02-21 13:19:54 -0800

    Now saves clips of video with no sound

--- Interpolation/src/interpolation/Interpolation.java
+++ Interpolation/src/interpolation/Interpolation.java
@@ -1,21 +1,45 @@
 package interpolation;
 
+import java.util.Vector;
 import org.opencv.core.Core;
 import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.core.Mat;
+import javax.media.MediaLocator;
 
 class InterpolateDemo {
   public void run() {
-    VideoCapture vc = loadVideo("/resources/videos/smw.avi");
+    
+    final int START_VIDEO = 100, END_VIDEO = 200;
+          
+    VideoCapture vc = loadVideo("/resources/videos/bbt.avi");
     Mat frame = new Mat();
-    for(int i = 0; i < 165; i++) vc.read(frame);
-    showFrame(frame);
+    Vector fileList = new Vector();
+    for(int i = 0; i < END_VIDEO; i++) {
+        vc.read(frame);
+        if(i < START_VIDEO) continue;
+        String fileName = "out/images/" + i + ".jpg";
+        fileList.add(fileName);
+        showFrame(frame, fileName);
+    }
+    JpegImagesToMovie imagesToMovie = new JpegImagesToMovie();
+    MediaLocator oml;
+    String fileName = "out/movie.mp4";
+    if ((oml = imagesToMovie.createMediaLocator(fileName)) == null) {
+        System.err.println("Cannot build media locator from: " + fileName);
+        System.exit(0);
+    }
+    try {
+        imagesToMovie.doIt(frame.width(), frame.height(), 48, fileList, oml);
+    } catch(Exception ex) {
+        System.out.println("Error saving video: " + ex.getMessage());
+    }
+    
     vc.release();
   }
   
-  private void showFrame(Mat frame) {
-      Highgui.imwrite("result.png", frame);
+  private void showFrame(Mat frame, String fileName) {
+      Highgui.imwrite(fileName, frame);
   }
   
   private VideoCapture loadVideo(String fileName) {
